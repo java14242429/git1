daa27d99d3557b952ff0713ed164a730d23a43e6
laiyifeng <laiyifeng>
2016-05-08 16:49:56 +0800

    '第三次提交'

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
     public static void main(){
-	    System.out.println("hello");
-		System.out.println("world");
+	    int a = 3;
+		int b = 4;
+		System.out.println(a+b);
 	}
 }
